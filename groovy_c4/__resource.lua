825034c0b64fdb5104c8480485d31e366908df7a
loco <43757529+Loc0@users.noreply.github.com>
2022-07-08 01:44:02 +0200

    Add files via upload

---
+++ groovy_c4/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+fx_version 'cerulean'
+game { 'gta5' }
+author 'loco'
+
+files {
+        "stream/*.ymap",
+        "stream/*.ytd",
+        "stream/*.ydr",
+        "stream/*.ybn",
+        "stream/*.ytyp", 
+        "stream/*.ymf",
+        "stream/*.yft",
+    }
+
+data_file 'DLC_ITYP_REQUEST' 'stream/groovy_c4.ydr'
